014c599c487e19f3f5b135219adfc1e4e6cbe019
stoppedwumm <mobileboxnews@icloud.com>
2024-10-07 15:11:54 +0200

    Add comment

--- config.js
+++ config.js
@@ -7,6 +7,11 @@ if (!config.enableArch) {
 }
 
 console.log("Executed")
+    /**
+     * Arch button click handler.
+     * If config.enableArch is true, opens the arch Eaglercraft client.
+     * If config.enableArch is false, shows an alert dialog saying "Arch is disabled".
+     */
 document.getElementById("archB").onclick = () => {
     console.log("Arch")
     if (config.enableArch) {
