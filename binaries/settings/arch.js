3e54ad18dd3474f07192da3d22a368d2011bb854
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2025-03-24 17:41:37 +0000

    Upgrade from hardcoded to general configs

---
+++ binaries/settings/arch.js
@@ -0,0 +1,24 @@
+"use strict";
+var relayId = Math.floor(Math.random() * 3);
+
+// %%%%%%%%% launch options %%%%%%%%%%%%
+
+export default {
+	hintsVersion: 1,
+	container: "game_frame",
+	worldsDB: "worlds",
+	relays: [
+		{ addr: "wss://relay.deev.is/", comment: "lax1dude relay #1", primary: relayId === 0 },
+		{ addr: "wss://relay.lax1dude.net/", comment: "lax1dude relay #2", primary: relayId === 1 },
+		{ addr: "wss://relay.shhnowisnottheti.me/", comment: "ayunami relay #1", primary: relayId === 2 }
+	],
+	checkRelaysForUpdates: true,
+	servers: [
+        { addr: "wss://mc.arch.lol/", name: "Arch MC" }
+    ],
+	allowBootMenu: false,
+	enableMinceraft: false,
+	joinServer: "wss://mc.arch.lol/"
+};
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
